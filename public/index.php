b4e3caed4b5ec255e9ce767bc199c8acfcc84131
mzeus98 <mzeus98@gmail.com>
2022-05-06 16:11:25 +0430

    adding addTODO section

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@ require_once __DIR__ . "/../vendor/autoload.php";
 // define("ROOT", $_SERVER['DOCUMENT_ROOT']);
 use App\core\Application;
 use App\app\renderView;
-use App\app\HomeController;
-use App\app\TodoController;
+use App\app\Controllers\HomeController;
+use App\app\Controllers\TodoController;
 
 $app = new Application(__DIR__);
 
@@ -15,8 +15,10 @@ $app->router->get('/', [HomeController::class, 'index']);
 $app->router->get('/GetAll', [TodoController::class, 'GetAll']);
 $app->router->get('/GetByID', [TodoController::class, 'GetByID']);
 $app->router->get('/AddTODO', [TodoController::class, 'AddTODO']);
-$app->router->post('/SendForm', [TodoController::class, 'SendForm']);
+$app->router->get('/SendForm', [TodoController::class, 'SendForm']);
 $app->router->post('/Toggle', [TodoController::class, 'Toggle']);
+$app->router->get('/Mohammad', [TodoController::class, 'mohammad']);
+
 /*  $app->router->get('/contact', 'contact');
  $app->router->get('/home', 'home');
  $app->router->get('/slm', 'slm');
--- view/AddTODO.php
+++ view/AddTODO.php
@@ -1,1 +1,17 @@
-<h3>Add TODO</h3>
+<div class="row">
+    <div class="col col-3"></div>
+    <div class="col col-6">
+
+    <form action="/SendForm" method="GET">
+    <div class="mb-3">
+        <label class="form-label">Todo Title</label>
+        <input type="text" name="todoTitle" class="form-control" >
+    </div>
+
+    <button type="submit" name="submit" class="btn btn-primary">Send</button>
+    </form>
+
+
+    </div>
+    <div class="col col-3"></div>
+</div>
